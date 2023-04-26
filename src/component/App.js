7c30c8756c9c14980502b88eb7ee9bc6d1fedac2
minwachya <minwachya@gmail.com>
2023-04-26 16:50:07 +0900

    [remove] component 파일들을 component 폴더 밑으로 이동

--- src/component/App.js
+++ src/component/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 import KeyRing from './KeyRing';
 import AddKeyRing from './AddKeyRing';
 import SearchKeyRing from './SearchKeyRing';
