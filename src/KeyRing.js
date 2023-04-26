e171cbe3190232eb0ae627ff33781e72ef2c119f
minwachya <minwachya@gmail.com>
2023-04-26 15:37:14 +0900

    [feat] KeyRing Component 생성

---
+++ src/KeyRing.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class KeyRing extends React.Component {
+    render() {
+        return (
+            <div className="KeyRing">
+                <img 
+                    src="https://onemorebag.kr/web/upload/NNEditor/20230410/IMG_4258.jpg" 
+                    alt="키링 이미지"
+                    width="200" 
+                    height="200"
+                    />
+                <label for="kr1">푸딩 키링</label>
+            </div>
+        );
+    }
+}
+
+export default KeyRing;
