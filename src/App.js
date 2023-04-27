1f04c38bc7bcf8aca311018abb7d24d041a4d481
minwachya <minwachya@gmail.com>
2023-04-27 21:40:54 +0900

    [feat] 키링 생성 서버 연결 성공

--- src/App.js
+++ src/App.js
@@ -2,39 +2,35 @@ import React from 'react';
 import './App.css';
 import KeyRing from './component/KeyRing';
 import Menu from './component/Menu';
+import { call } from "./service/ApiService";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [
-        {id:"id1", title:"강아지 키링", userId: "MinYoung", detail:"멍멍이 소리가 나요", imgUrl: "https://onemorebag.kr/web/upload/NNEditor/20230410/IMG_4258.jpg" },
-        {id:"id2", title:"고양이 키링", userId: "MinYoung", detail:"야옹이 소리가 나요", imgUrl: "https://onemorebag.kr/web/upload/NNEditor/20230410/IMG_4258.jpg" },
-      ]
+      items: []
     };
   }
-  
-  // 키링 생성
+
+  componentDidMount() {
+    call("/keyring", "GET", null).then((response) => 
+      this.setState({items: response.data})
+    );
+  }
+
+  // 키링 추가: Menu의 AddKeyRing에 전달
   add = (item) => {
-    const tempItems = this.state.items;
-    item.id = "id" + tempItems.length;
-    tempItems.push(item);
-    this.setState({items: tempItems});
-  }
-
-  // 키링 삭제
-  delete = (title) => {
-    const tempItems = this.state.items;
-    const newItems = tempItems.filter(e => e.title !== title);
-    this.setState({items: newItems});
-  }
-
-  // 키링 수정
-  modify = (item) => {
-    const tempItems = this.state.items;
-    tempItems.find(e => e.id !== item.id).modify({item});
-    this.setState({items: tempItems});
-  }
+    call("/keyring", "POST", item).then((response) => 
+      this.setState({items: response.data})
+    );
+  };
+  
+  // 키링 삭제: Menu의 DeleteKeyRing에 전달
+  delete = (item) => {
+    call("/keyring", "DELETE", item).then((response) => 
+      this.setState({items: response.data})
+    );
+  };
 
   render() {
     var keyRingItems = this.state.items.map(
@@ -48,7 +44,6 @@ class App extends React.Component {
         <Menu 
           add={this.add}
           delete={this.delete}
-          modify={this.modify}
         />
         {keyRingItems}
       </div>
