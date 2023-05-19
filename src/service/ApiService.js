e9ee86e8e0fdd90e774b1eafed82eae7bf301f08
minwachya <minwachya@gmail.com>
2023-05-19 18:56:51 +0900

    [feat] ApiService에 로그인, 로그아웃, 회원가입 함수 추가 및 요청 보낼 때 헤더에 토큰 보내는 코드 추가

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,26 +1,64 @@
 import { API_BASE_URL } from "../app-config"
+const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 // Promise 객체 리턴
 export function call(api, method, request) {
+    // 토큰을 헤더에 추가
+    let headers = new Headers({
+        "Content-Type": "application/json",
+    });
+    const accessToken = localStorage.getItem(ACCESS_TOKEN);
+    if(accessToken && accessToken !== null) {
+        headers.append("Authorization", "Bearer " + accessToken);
+    }
     let options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
+    
     // 요청 데이터 있으면 body에 담음
     if(request) {
         options.body = JSON.stringify(request); // 문자열로 변환
     }
 
     // 서버에 요청 후 응답 받아옴. Promise 반환함
-    return fetch(options.url, options).then((response) => 
-        response.json().then((json) => {
-            if(!response.ok) {
-                return Promise.reject(json);
+    return fetch(options.url, options)
+        .then((response) => 
+            response.json().then((json) => {
+                if(!response.ok) {
+                    return Promise.reject(json);
+                }
+                return json;
+            })
+        )
+        // 로그인 안 한 상태일 때 로그인 화면으로 리다이렉트
+        .catch((error) => {
+            console.log(error.status);
+            if(error.status === 403) {
+                window.location.href = "/login"; // redirect
             }
-            return json;
-        })
-    );
+            return Promise.reject(error);
+        });
 }
+
+export function signin(userDTO) {
+    return call("/auth/signin", "POST", userDTO)
+        .then((response) => {
+            // 토큰 저장
+            localStorage.setItem(ACCESS_TOKEN, response.token);
+            // 리다이렉트
+            window.location.href = "/";
+        });
+    }
+
+export function signup(userDTO) {
+    return call("/auth/signup", "POST", userDTO);
+    }
+
+export function signout() {
+    // 토큰 삭제
+    localStorage.setItem(ACCESS_TOKEN, null);
+    // 리다이렉트
+    window.location.href = "/login";
+    }
