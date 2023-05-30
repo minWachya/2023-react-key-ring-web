1f363a9d36969ff5160d9e6d883b92d6cfd82ccb
minwachya <minwachya@gmail.com>
2023-05-30 21:29:16 +0900

    [ui] 로그인 ui 수정

--- src/ui/Login.js
+++ src/ui/Login.js
@@ -1,7 +1,28 @@
 import React from "react";
 import { signin } from "../service/ApiService";
-import { Button, TextField, Link, Grid, Container, Typography } from "@material-ui/core";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
 
+function Copyright(props) {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="https://min-wachya.tistory.com/">
+          20190054 이민영 
+        </Link>
+         {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
 
 class Login extends React.Component {
     constructor(props) {
@@ -23,50 +44,81 @@ class Login extends React.Component {
 
     render() {
         return (
-            <Container component="main" maxWidth="xs" style={{marginTop: "8%"}}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        {/* h5 스타일 적용 후 최종적으로 h1로 만들어라 */}
-                        <Typography component="h1" variant="h5">로그인</Typography>
-                    </Grid>
-                </Grid>
-
-                <form noValidate onSubmit={this.handleSubmit}>
-                    {" "} {/* 간격 띄우기 */}
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="email"
-                                label="이메일 주소"
-                                name="email"
-                                autoComplete="email" />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="password"
-                                label="비밀번호"
-                                name="password"
-                                autoComplete="current-pasword" />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                color="primary">로그인</Button>
-                        </Grid>
+            <Grid container component="main" sx={{ height: '100vh' }}>
+              <CssBaseline />
+              <Grid
+                item
+                xs={false}
+                sm={4}
+                md={7}
+                sx={{
+                  backgroundImage: 'url(https://pbs.twimg.com/media/FxGMQwcaYAAioTU?format=jpg&name=large)',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundColor: (t) =>
+                    t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                }}
+              />
+              <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <Box
+                  sx={{
+                    my: 8,
+                    mx: 4,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                  }}
+                >
+                  <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                  </Avatar>
+                  <Typography component="h1" variant="h5">
+                    로그인
+                  </Typography>
+                  <Box component="form" noValidate onSubmit={this.handleSubmit} sx={{ mt: 1 }}>
+                    <TextField
+                      margin="normal"
+                      required
+                      fullWidth
+                      id="email"
+                      label="이메일 주소"
+                      name="email"
+                      autoComplete="email"
+                      autoFocus
+                    />
+                    <TextField
+                      margin="normal"
+                      required
+                      fullWidth
+                      name="password"
+                      label="비밀번호"
+                      type="password"
+                      id="password"
+                      autoComplete="current-password"
+                    />
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      sx={{ mt: 3, mb: 2 }}
+                    >
+                      로그인
+                    </Button>
+                    <Grid container>
+                      <Grid item xs>
+                      </Grid>
+                      <Grid item>
                         <Link href="/signup" variant="body2">
-                            <Grid item>계정이 없습니까? 여기서 가입 하세요.</Grid>
-                        </Link> 
+                          {"회원가입"}
+                        </Link>
+                      </Grid>
                     </Grid>
-                </form>
-            </Container>
+                    <Copyright sx={{ mt: 5 }} />
+                  </Box>
+                </Box>
+              </Grid>
+            </Grid>
         );
     }
 }
